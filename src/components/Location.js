26cbfc5a95e805178393b442253016d01dac1a9b
aleksey <mirovskyi@gmail.com>
2017-12-18 09:56:55 +0200

    View locatio details

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,6 +3,7 @@ import {Grid} from "semantic-ui-react";
 import Country from "./Location/Country";
 import Region from "./Location/Region";
 import City from "./Location/City";
+import "whatwg-fetch";
 
 export default class Location extends React.Component {
 
@@ -11,19 +12,21 @@ export default class Location extends React.Component {
         this.state = {
             country: null,
             region: null,
-            city: null
+            city: null,
+            cityData: {}
         };
     }
 
     handleChangeCountry = (country) => {
-        this.setState({country, region: null, city: null});
+        this.setState({country, region: null, city: null, cityData: {}});
     };
 
     handleChangeRegion = (region) => {
-        this.setState({region, city: null});
+        this.setState({region, city: null, cityData: {}});
     };
 
     handleChangeCity = (city) => {
+        this.fetchCityData(city);
         this.setState({city});
     };
 
@@ -55,5 +58,17 @@ export default class Location extends React.Component {
                 </Grid.Row>
             </Grid>
         );
+    }
+
+    fetchCityData(id) {
+        fetch(`${process.env.REACT_APP_GOPLACES_URL}/api/v1/city/${id}`)
+            .then(response => response.json())
+            .then(cityData => {
+                this.setState({cityData})
+            })
+            .catch(() => {
+                this.setState({cityData: {}})
+            })
+        ;
     }
 }
